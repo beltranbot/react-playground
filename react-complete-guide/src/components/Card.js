70a8356362b419ce9dbaca614e95d431e26d2aef
beltranbot <beltranbot@email.com>
2022-02-12 18:42:57 +0000

    fixed typo

--- react-complete-guide/src/components/Card.js
+++ react-complete-guide/src/components/Card.js
@@ -2,7 +2,7 @@ import './Card.css';
 
 function Card(props) {
   const classes = 'card ' + props.className;
-  // propts will always have the content within the opening and closing tabs
+  // props will always have the content within the opening and closing tabs
   return (
     <div className={classes}>
       {props.children}
