38342a2ee799e184c7758b681296e5cb61d0d611
beltranbot <beltranbot@email.com>
2022-02-12 18:18:19 +0000

    splitting components, adding expensedate component

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,17 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   const { expense } = props;
-  const month = expense.date.toLocaleString("en-US", { month: "long" });
-  const day = expense.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = expense.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={expense.date} />
       <div className="expense-item__description">
         <h2>{expense.title}</h2>
         <div className="expense-item__price">$ {expense.amount}</div>
