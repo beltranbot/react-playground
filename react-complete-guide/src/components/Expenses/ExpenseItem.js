b06ed1f6557da8878f258b6c90d3b165002f6c01
beltranbot <beltranbot@email.com>
2022-02-12 19:40:00 +0000

    adding expense form

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,30 +1,18 @@
-import { useState } from 'react'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  // title is a pointer to the managed value
-  // setTitle is the setter function for the value
-  // useState always return 2 values
-  const [title, setTitle] = useState(props.title) // using a react hook, should be called at this level
-
   const amount = '$' + props.amount
-  const clickHandler = () => {
-    // you must call the set function to update the UX
-    setTitle('Updated!')
-    console.log(title) // the title would still be the original
-    // setTitle schedules the title to update, but doesn't update it right away
-  }
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>{amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
+      <button>Change title</button>
     </Card>
   );
 }
