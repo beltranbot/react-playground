f429fc62190935e86ae5149ef3c6f588b46fb78a
beltranbot <beltranbot@gmail.com>
2022-02-22 19:58:26 +0000

    adding reusable

--- 03-example01/src/components/Users/AddUser.js
+++ 03-example01/src/components/Users/AddUser.js
@@ -1,16 +1,22 @@
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css"
+
 const AddUser = props => {
   const addUserHandler = (event) => {
     event.preventDefaul();
   }
-
+  // applying 2 css classes
   return (
-    <form onSubmit={addUserHandler}>
-      <label htlFor="username">Username</label>
-      <input id="username" type="text"></input>
-      <label htlFor="age">Age (years)</label>
-      <input id="age" type="number"></input>
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}> 
+      <form onSubmit={addUserHandler}>
+        <label htlFor="username">Username</label>
+        <input id="username" type="text"></input>
+        <label htlFor="age">Age (years)</label>
+        <input id="age" type="number"></input>
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
   )
 };
 
