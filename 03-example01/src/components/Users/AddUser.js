3d4655be5e13473b59c348aed22934db7996acc3
beltranbot <beltranbot@gmail.com>
2022-02-23 14:15:51 +0000

    adding wrapper component

--- 03-example01/src/components/Users/AddUser.js
+++ 03-example01/src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Wrapper from "../Helpers/Wrapper";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
@@ -43,7 +44,7 @@ const AddUser = (props) => {
 
   // applying 2 css classes
   return (
-    <div>
+    <Wrapper>
       {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
@@ -64,7 +65,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
