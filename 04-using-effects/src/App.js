1908aa240ff5e5d7cc11b51b42240faa977cd85f
beltranbot <beltranbot@gmail.com>
2022-03-02 21:59:44 +0000

    building and using a custom context provider component

--- 04-using-effects/src/App.js
+++ 04-using-effects/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -6,40 +6,15 @@ import MainHeader from "./components/MainHeader/MainHeader";
 import AuthContext from "./store/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // useEffect runs after all the component have loaded
-  // it only runs once
-  // only runs again if the dependencies, the second argument, changes
-  useEffect(() => {
-    const storeUserLoggedInInformation = localStorage.getItem("isLoggedIn");
-    if (storeUserLoggedInInformation === "1") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.setItem("isLoggedIn", "0");
-    setIsLoggedIn(false);
-  };
-
+  const ctx = useContext(AuthContext);
   return (
-    <AuthContext.Provider value={{
-      isLoggedIn: isLoggedIn,
-      onLogout: logoutHandler
-    }}>
-      <MainHeader onLogout={logoutHandler} />
+    <React.Fragment>
+      <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home onLogout={ctx.onLogout} />}
       </main>
-    </AuthContext.Provider>
+    </React.Fragment>
   );
 }
 
--- 04-using-effects/src/components/Login/Login.js
+++ 04-using-effects/src/components/Login/Login.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useReducer } from "react";
 
 import Card from "../UI/Card/Card";
+import { useContext } from 'react';
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -24,7 +26,7 @@ const passwordReducer = (state, action) => {
   return { value: "", isValid: false };
 };
 
-const Login = (props) => {
+const Login = () => {
   // const [enteredEmail, setEnteredEmail] = useState("");
   // const [emailIsValid, setEmailIsValid] = useState();
   // const [enteredPassword, setEnteredPassword] = useState("");
@@ -39,6 +41,8 @@ const Login = (props) => {
     value: "",
     isValid: null,
   });
+
+  const authCtx = useContext(AuthContext);
 
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
@@ -83,7 +87,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
