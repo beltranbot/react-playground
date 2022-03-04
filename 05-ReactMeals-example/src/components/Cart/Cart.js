5158386397b69fb3e1c0ff80a4e2fea8708149ac
beltranbot <beltranbot@gmail.com>
2022-03-04 15:44:03 +0000

    adding a portal via react portal

--- 05-ReactMeals-example/src/components/Cart/Cart.js
+++ 05-ReactMeals-example/src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
@@ -14,7 +15,7 @@ const Cart = (props) => {
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -24,7 +25,7 @@ const Cart = (props) => {
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
