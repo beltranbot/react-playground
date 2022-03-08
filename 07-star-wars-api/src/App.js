b202272f72fa378fd33910f494f79c4653465aba
beltranbot <beltranbot@gmail.com>
2022-03-08 21:52:48 +0000

    handling http errors

--- 07-star-wars-api/src/App.js
+++ 07-star-wars-api/src/App.js
@@ -6,21 +6,40 @@ import './App.css';
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchMoviesHandler = async () => {
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films/');
-    const data = await response.json();
-    const transformedMovies = data.results.map(movie => {
-      return {
-        id: movie.episode_id,
-        title: movie.title,
-        openingText: movie.opening_crawl,
-        releaseDate: movie.release_date
-      };
-    });
-    setMovies(transformedMovies);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Something went wrong')
+      }
+      const data = await response.json();
+      const transformedMovies = data.results.map(movie => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (err) {
+      setError(err.message);
+    }
     setIsLoading(false);
+  }
+
+  let content = <p>Found no movies.</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+  if (error) {
+    content = <p>{error}</p>;
   }
 
   return (
@@ -29,9 +48,7 @@ const App = () => {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-        {isLoading && <p>Loading...</p>}
+        {content}
       </section>
     </React.Fragment>
   );
