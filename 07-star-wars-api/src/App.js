5e5356dd4ad3e1f844918436eff6e845deac5367
beltranbot <beltranbot@gmail.com>
2022-03-08 20:36:10 +0000

    using async await

--- 07-star-wars-api/src/App.js
+++ 07-star-wars-api/src/App.js
@@ -3,27 +3,21 @@ import React, { useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
-function App() {
+const App = () => {
   const [movies, setMovies] = useState([]);
-  const fetchMoviesHandler = () => {
-    fetch('https://swapi.dev/api/films/')
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        const transformedMovies = data.results.map(movie => {
-          return {
-            id: movie.episode_id,
-            title: movie.title,
-            openingText: movie.opening_crawl,
-            releaseDate: movie.release_date
-          };
-        });
-        setMovies(transformedMovies);
-      })
-      .catch(err => console.error(err));
-  };
-
+  const fetchMoviesHandler = async () => {
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json();
+    const transformedMovies = data.results.map(movie => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date
+      };
+    });
+    setMovies(transformedMovies);
+  }
   return (
     <React.Fragment>
       <section>
@@ -34,6 +28,6 @@ function App() {
       </section>
     </React.Fragment>
   );
-}
+};
 
 export default App;
